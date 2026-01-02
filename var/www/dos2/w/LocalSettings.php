cdf77f57fab2d17aad4968f9798970f042fd4192
Matt-Hurd <matt-hurd@users.noreply.github.com>
2026-01-01 23:35:40 -0800

    update

--- var/www/dos2/w/LocalSettings.php
+++ var/www/dos2/w/LocalSettings.php
@@ -1,7 +1,9 @@
 <?php
 require "/var/www/dos2/.secrets.php";
 
-$devSite = (($_COOKIE['dos2wiki_dev'] ?? '') === $devModePwd);
+// $devSite = (($_COOKIE['dos2wiki_dev'] ?? '') === $devModePwd);
+
+$devSite = true;
 
 if ( $devSite ) {
 	require 'LocalSettings.dev.php';
